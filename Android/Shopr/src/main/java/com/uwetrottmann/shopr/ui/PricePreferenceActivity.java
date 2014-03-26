284476f68b6f10da232f97c7238fc56b1d8b7838
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-03-26 21:14:42 +0100

    Added forgotten PricePreferenceActivity file in the previous commit.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/PricePreferenceActivity.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/PricePreferenceActivity.java
@@ -1,18 +1,10 @@
 package com.uwetrottmann.shopr.ui;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -23,95 +15,30 @@ import com.uwetrottmann.shopr.algorithm.model.Attributes.Attribute;
 import com.uwetrottmann.shopr.algorithm.model.Attributes.AttributeValue;
 import com.uwetrottmann.shopr.algorithm.model.Price;
 
-public class PricePreferenceActivity extends Activity implements
-		OnItemClickListener {
+public class PricePreferenceActivity extends PreferenceActivity {
 
-	protected TextView mTextViewExplanation;
-	protected ListView mListView;
-	protected Button mButtonUpdatePreferences;
-	protected ArrayAdapter<AttributeValue> mAdapter;
-
-	protected Attribute attribute() {
+	protected final Attribute attribute() {
 		return new Price();
 	}
-
-	protected void onUpdatePreferencesFinish() {
-
+	
+	protected final int layout() {
+		return R.layout.price_preference;
+	}
+	
+	protected final TextView explanationView() {
+		return (TextView) findViewById(R.id.textViewAttributeValuePreference);
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.price_preference);
-		setupViews();
+	protected final Button updatePreferenceButton() {
+		return (Button) findViewById(R.id.buttonUpdatePreferences);
 	}
 
-	protected void setupViews() {
-		mTextViewExplanation = (TextView) findViewById(R.id.textViewAttributeValuePreference);
-		mTextViewExplanation.setText(getPreferenceExplanation());
-
-		mButtonUpdatePreferences = (Button) findViewById(R.id.buttonUpdatePreferences);
-		mButtonUpdatePreferences.setEnabled(false);
-		mButtonUpdatePreferences.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				onUpdateAttributeValuePreferences();
-			}
-		});
-
-		mAdapter = new PriceRangeAdapter(this);
-		mAdapter.clear();
-		mAdapter.addAll(attribute().getAttributeValues());
-
-		mListView = (ListView) findViewById(R.id.listViewAttributeValueList);
-		mListView.setOnItemClickListener(this);
-		mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-		
-		mListView.setAdapter(mAdapter);
+	protected final ArrayAdapter<AttributeValue> attributeValueAdapter() {
+		return new PriceRangeAdapter(this);
 	}
 
-	protected int numColums() {
-		return 4;
-	}
-
-	protected String getPreferenceExplanation() {
-		String baseExplanation = getString(R.string.attribute_value_preference_update_explanation);
-		return String.format(baseExplanation, attribute().id());
-	}
-
-	protected void onUpdateAttributeValuePreferences() {
-		SparseBooleanArray checkedPositions = mListView
-				.getCheckedItemPositions();
-		Set<AttributeValue> attributeValues = new HashSet<AttributeValue>();
-		for (int i = 0; i < mAdapter.getCount(); i++) {
-			if (checkedPositions.get(i)) {
-				attributeValues.add(mAdapter.getItem(i));
-			}
-		}
-		onUpdatePreferencesFinish();
-	}
-
-	@Override
-	public void onItemClick(AdapterView<?> a, View v, int arg2, long arg3) {
-		mButtonUpdatePreferences.setEnabled(isButtonEnabled());
-	}
-
-	private boolean isButtonEnabled() {
-		return hasAtLeastOneItemChecked();
-	}
-
-	private boolean hasAtLeastOneItemChecked() {
-		boolean itemChecked = false;
-		SparseBooleanArray checkedPositions = mListView
-				.getCheckedItemPositions();
-		for (int i = 0; i < checkedPositions.size(); i++) {
-			if (checkedPositions.valueAt(i)) {
-				itemChecked = true;
-				break;
-			}
-		}
-
-		return itemChecked;
+	protected final AbsListView attributeValueList() {
+		return (ListView) findViewById(R.id.listViewAttributeValueList);
 	}
 
 	public class PriceRangeAdapter extends ArrayAdapter<AttributeValue> {
