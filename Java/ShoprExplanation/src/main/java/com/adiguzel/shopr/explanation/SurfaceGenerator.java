908ca21eab0d479400d86625ea8582413d1d0008
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-15 13:33:29 +0200

    Added SurfaceGenerator.

---
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/SurfaceGenerator.java
@@ -0,0 +1,12 @@
+package com.adiguzel.shopr.explanation;
+
+import com.adiguzel.shopr.explanation.model.Explanation;
+import com.uwetrottmann.shopr.algorithm.Query;
+
+public class SurfaceGenerator {
+	
+	public Explanation generate(Explanation abstractExplanation, Query query) {
+		return null;
+	}
+
+}
