008362a737bc7f7a783d84c6a03e6dab84d16242
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-15 13:31:11 +0200

    Added Argument.Type.

--- Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Argument.java
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Argument.java
@@ -3,10 +3,16 @@ package com.adiguzel.shopr.explanation.model;
 public class Argument implements Comparable<Argument>{
 	private Dimension dimension;
 	private boolean isPositive;
+	private Type type;
+	
+	public Argument(Type type) {
+		this.type = type;
+	}
 
 	public Argument(Dimension dimension, boolean isPositive) {
 		this.dimension = dimension;
 		this.isPositive = isPositive;
+		this.type = Type.ON_DIMENSION;
 	}
 
 	public boolean isPositive() {
@@ -26,4 +32,13 @@ public class Argument implements Comparable<Argument>{
 		return this.dimension.compareTo(arg.dimension);
 	}
 	
+	public Type getType() {
+		return type;
+	}
+	
+	public enum Type {
+		ON_DIMENSION,
+		GOOD_AVERAGE,
+		NO_BETTER_ALTERNATIVES
+	}
 }
