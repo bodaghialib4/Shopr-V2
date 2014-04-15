ab24785225d885e4c39d7cf5f80a1047684ed7b2
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-15 15:42:39 +0200

    Seperated primary arguments from supporting arguments.

--- Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Explanation.java
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Explanation.java
@@ -1,5 +1,6 @@
 package com.adiguzel.shopr.explanation.model;
 
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -7,26 +8,44 @@ import com.uwetrottmann.shopr.algorithm.model.Item;
 
 public class Explanation {
 	private Item item;
-	private Set<Argument> arguments;
+	private Set<Argument> primaryArguments;
+	private Set<Argument> supportingArguments;
 
 	public Explanation(Item item) {
 		this.item = item;
-		arguments = new LinkedHashSet<Argument>();
+		primaryArguments = new LinkedHashSet<Argument>();
+		supportingArguments = new LinkedHashSet<Argument>();
 	}
 
-	public Set<Argument> arguments() {
-		return arguments;
+	public Set<Argument> primaryArguments() {
+		return primaryArguments;
+	}
+	
+	public Set<Argument> supportingArguments() {
+		return supportingArguments;
 	}
 
 	public Argument mainArgument() {
-		if (arguments.iterator().hasNext())
-			return arguments.iterator().next();
+		if (primaryArguments.iterator().hasNext())
+			return primaryArguments.iterator().next();
 		else
 			return null;
 	}
 
-	public void addArgument(Argument argument) {
-		arguments.add(argument);
+	public void addPrimaryArgument(Argument argument) {
+		primaryArguments.add(argument);
+	}
+	
+	public void addPrimaryArguments(Collection<Argument> arguments) {
+		primaryArguments.addAll(arguments);
+	}
+	
+	public void addSupportingArgument(Argument argument) {
+		supportingArguments.add(argument);
+	}
+	
+	public void addSupportingArguments(Collection<Argument> arguments) {
+		supportingArguments.addAll(arguments);
 	}
 
 	public Item item() {
