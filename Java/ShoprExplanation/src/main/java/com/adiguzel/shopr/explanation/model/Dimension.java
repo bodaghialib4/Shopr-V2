e181b1511030a3640b70a4c301bf6fd994152fde
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-21 14:31:48 +0200

    Added hybrid score to Dimension.

--- Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Dimension.java
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Dimension.java
@@ -7,29 +7,33 @@ public class Dimension {
 	private Attribute attribute;
 	private double explanationScore;
 	private double informationScore;
-	
+
 	public Dimension(Attribute attribute) {
 		this.attribute = attribute;
 	}
-	
+
+	public double hybridScore() {
+		return (explanationScore + informationScore) / 2;
+	}
+
 	public Attribute attribute() {
 		return attribute;
 	}
-	
+
 	public Dimension explanationScore(double explanationScore) {
 		this.explanationScore = explanationScore;
 		return this;
 	}
-	
+
 	public double explanationScore() {
 		return explanationScore;
 	}
-	
+
 	public Dimension informationScore(double informationScore) {
 		this.informationScore = informationScore;
 		return this;
 	}
-	
+
 	public double informationScore() {
 		return informationScore;
 	}
