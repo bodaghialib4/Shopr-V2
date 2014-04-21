c5adb77dabf445603938a134ed0f885196f33c1f
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-04-21 14:34:04 +0200

    Added list of current user context as part of the interfaces in Discloser.

--- Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/Discloser.java
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/Discloser.java
@@ -3,7 +3,7 @@ package com.adiguzel.shopr.explanation;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.adiguzel.shopr.explanation.model.Argument;
+import com.adiguzel.shopr.explanation.model.Context;
 import com.adiguzel.shopr.explanation.model.Explanation;
 import com.uwetrottmann.shopr.algorithm.AdaptiveSelection;
 import com.uwetrottmann.shopr.algorithm.Query;
@@ -12,19 +12,19 @@ import com.uwetrottmann.shopr.algorithm.model.Item;
 
 public class Discloser {
 
-	public List<Recommendation> explain(List<Item> recommendedItems, Query query) {
+	public List<Recommendation> explain(List<Item> recommendedItems, Query query, List<Context> contexts) {
 		List<Recommendation> explainedRecommendations = new ArrayList<Recommendation>();
 		for (Item item : recommendedItems) {
 			explainedRecommendations.add(new Recommendation(item, explain(item,
-					query, recommendedItems)));
+					query, recommendedItems, contexts)));
 		}
 		return explainedRecommendations;
 	}
 
 	private Explanation explain(Item item, Query query,
-			List<Item> recommendedItems) {
+			List<Item> recommendedItems, List<Context> contexts) {
 		Explanation abstractExplanation = new ContentSelector().select(item,
-				query, recommendedItems);
+				query, recommendedItems, contexts);
 		return abstractExplanation;
 		//return new SurfaceGenerator().generate(abstractExplanation, query);
 	}
@@ -32,17 +32,17 @@ public class Discloser {
 	public static void main(String[] args) {
 		AdaptiveSelection as = AdaptiveSelection.get();
 		as.setInitialCaseBase(Utils.getLimitedCaseBase(), true);
-		for (Recommendation r : new Discloser().explain(as.getRecommendations(),
+		/*for (Recommendation r : new Discloser().explain(as.getRecommendations(),
 				as.getCurrentQuery())) {
 			System.out.println("" + r.item().name());
 			System.out.println("Primary");
-			for (Argument arg : r.explanation().primaryArguments()) {
+			for (DimensionArgument arg : r.explanation().primaryArguments()) {
 				System.out.println("" + arg.getType());
 			}
 			System.out.println("Secondary");
-			for (Argument arg : r.explanation().supportingArguments()) {
+			for (DimensionArgument arg : r.explanation().supportingArguments()) {
 				System.out.println("" + arg.getType());
 			}
-		}
+		}*/
 	}
 }
