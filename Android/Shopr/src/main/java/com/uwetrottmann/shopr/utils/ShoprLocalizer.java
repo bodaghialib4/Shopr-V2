c65fa1dc506a128852ad72d631cbf56be00da53b
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-05-02 21:13:59 +0200

    Expanded ShoprLocalizer with ExplanationLocalizer.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/utils/ShoprLocalizer.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/utils/ShoprLocalizer.java
@@ -3,10 +3,10 @@ package com.uwetrottmann.shopr.utils;
 
 import android.content.Context;
 
+import com.adiguzel.shopr.explanation.ExplanationLocalizer;
 import com.uwetrottmann.shopr.R;
-import com.uwetrottmann.shopr.algorithm.LocalizationModule;
 
-public class ShoprLocalizer implements LocalizationModule {
+public class ShoprLocalizer implements ExplanationLocalizer {
 
     private Context mContext;
 
@@ -33,5 +33,35 @@ public class ShoprLocalizer implements LocalizationModule {
     public String getPreferablyString() {
         return mContext.getString(R.string.preferably);
     }
+
+	@Override
+	public String getIndifferentToAnyTemplate() {
+		return mContext.getString(R.string.explanation_on_preference_indifferent);
+	}
+
+	@Override
+	public String getOnlySomeTemplate() {
+		return mContext.getString(R.string.explanation_on_preference_only_interested_in_some);
+	}
+
+	@Override
+	public String getAvoidsSomeTemplate() {
+		return mContext.getString(R.string.explanation_on_preference_avoiding_some);
+	}
+
+	@Override
+	public String getPrefersSomeTemplate() {
+		return mContext.getString(R.string.explanation_on_preference_prefer_some);
+	}
+
+	@Override
+	public String getCommaString() {
+		return mContext.getString(R.string.conjunction_comma);
+	}
+
+	@Override
+	public String getAndString() {
+		return mContext.getString(R.string.conjunction_and);
+	}
 
 }
