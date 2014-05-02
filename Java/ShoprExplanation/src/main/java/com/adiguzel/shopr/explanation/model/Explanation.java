2cc7743619bc24900c696ad955cd6a496f236831
Ugur Adiguzel <uguradgzl88@gmail.com>
2014-05-02 15:18:28 +0200

    Added Explanation, which encapsulates positive and negative reasons.

---
+++ Java/ShoprExplanation/src/main/java/com/adiguzel/shopr/explanation/model/Explanation.java
@@ -0,0 +1,37 @@
+package com.adiguzel.shopr.explanation.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Explanation {
+	private CharSequence simpleReason;
+	private Collection<CharSequence> positiveReasons = new ArrayList<CharSequence>();
+	private Collection<CharSequence> negativeReasons = new ArrayList<CharSequence>();
+
+	public Explanation addPositiveReason(CharSequence positive) {
+		positiveReasons.add(positive);
+		return this;
+	}
+
+	public Collection<CharSequence> positiveReasons() {
+		return positiveReasons;
+	}
+
+	public Explanation addNegativeReason(CharSequence negative) {
+		positiveReasons.add(negative);
+		return this;
+	}
+
+	public Collection<CharSequence> negativeReasons() {
+		return negativeReasons;
+	}
+
+	public Explanation simple(CharSequence simpleReason) {
+		this.simpleReason = simpleReason;
+		return this;
+	}
+
+	public CharSequence simple() {
+		return simpleReason;
+	}
+}
